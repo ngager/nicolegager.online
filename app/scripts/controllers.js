e2c110c414b6c7449fbe30a3c011c3a744793374
nicole.gager <nicole.gager@csquaredsystems.com>
2016-12-22 15:10:45 -0500

    Fixing Nepal Pics. Still need to cleanup code and refactor.

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,7 +4,14 @@
 'use strict';
 angular.module('nicolegager')
     .controller('IndexController', ['$scope', function ($scope) {
-        //TODO: implement controller
         $scope.avatar = '/nicolegager.online/app/img/avatar.JPG';
+        $scope.getNepalImages = function () {
+            var imageSource = '/nicolegager.online/app/img/nepal/';
+            var nepalMediaList = [];
+            for (var i = 1; i < 45; i++) {
+                nepalMediaList.push({src: imageSource + 'NEPAL-' + i + '.jpeg'});
+            }
+            return nepalMediaList;
+        };
     }])
 ;
