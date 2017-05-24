4ce4213bfbab5d957fbc685330243025f0e2bb70
nicole.gager <nicole.gager@csquaredsystems.com>
2017-05-24 12:20:27 -0400

    Remove controller logic for picture gallery as it will not be embedded till a later date

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,14 +4,6 @@
 'use strict';
 angular.module('nicolegager')
     .controller('IndexController', ['$scope', function ($scope) {
-        $scope.avatar = '/nicolegager.online/app/img/avatar.JPG';
-        $scope.getNepalImages = function () {
-            var imageSource = '/nicolegager.online/app/img/nepal/';
-            var nepalMediaList = [];
-            for (var i = 1; i < 45; i++) {
-                nepalMediaList.push({src: imageSource + 'NEPAL-' + i + '.jpeg'});
-            }
-            return nepalMediaList;
-        };
+
     }])
 ;
